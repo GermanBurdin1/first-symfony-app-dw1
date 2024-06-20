3edd7130627deee11e43ce621004dfe4de356656
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-06-20 19:13:43 +0200

    template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,22 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
-    {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
-    }
+	#[Route('/', name: 'app_home_index')]
+	public function index(): Response
+	{
+			return $this->render('home/index.html.twig', [
+					'prénom' => 'German',
+			]);
+	}
+
+	#[Route('/a-propos', name: 'app_home_about')]
+	public function about(): Response
+	{
+			return $this->render('home/about.html.twig');
+	}
+
 }
