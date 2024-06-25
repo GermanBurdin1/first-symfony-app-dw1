e89601fea2dd5647ed8c4df9f824457dce6bc6a1
German <125369843+GermanBurdin1@users.noreply.github.com>
2024-06-25 09:39:16 +0200

    first commit

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,25 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
-	#[Route('/', name: 'app_home_index')]
-	public function index(): Response
-	{
-			return $this->render('home/index.html.twig', [
-					'prénom' => 'German',
-			]);
-	}
+    #[Route('/', name: 'app_home_index')]
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->findAllWithTags();
+        return $this->render('home/index.html.twig', [
+            'posts' => $posts
+        ]);
+    }
 
-	#[Route('/a-propos', name: 'app_home_about')]
-	public function about(): Response
-	{
-			return $this->render('home/about.html.twig');
-	}
-
+    #[Route('/a-propos', name: 'app_home_about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig', [
+            'prenom' => 'Rémy',
+        ]);
+    }
 }
---
+++ src/Controller/PostController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PostController extends AbstractController
+{
+    #[Route('/articles', name: 'app_post_index')]
+    public function index(): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'controller_name' => 'PostController',
+        ]);
+    }
+
+    #[Route('/article/{id}', name: 'app_post_show')]
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+}
